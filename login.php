e2e75340b4599dbb290f923b16d015e8fb9c3b31
José Fernandes <zefrancisco2002@gmail.com>
2022-04-04 10:45:12 +0100

    Invalid Login

--- login.php
+++ login.php
@@ -12,13 +12,11 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         $_SESSION["username"]=$_POST['username'];
         header('Location: dashboard.php');
     } else {
-        echo "Login invalido..." . "<br>";
+        $invalido = "Login invalido...";
     }
 } else {
-    echo "Nada inserido..." . "<br>";
+    $inserido= "Nada inserido...";
 }
-
-
 ?>
 
 <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
@@ -29,7 +27,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 <html>
 
 <head>
-    <title>My Awesome Login Page</title>
+    <title>Login Inteligente</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" integrity="sha384-gfdkjb5BdAXd+lj+gudLWI+BXq4IuLW5IT+brZEZsLFm++aCMlF1V92rMkPaX4PP" crossorigin="anonymous">
@@ -52,7 +50,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                             </div>
                             <input type="text" id="username" name="username" class="form-control input_user" value="" placeholder="username">
                         </div>
-                        <div class="input-group mb-2">
+                        <div class="input-group mb-2">  
                             <div class="input-group-append">
                                 <span class="input-group-text"><i class="fas fa-key"></i></span>
                             </div>
@@ -61,6 +59,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
                         <div class="form-group">
                             <div class="custom-control custom-checkbox">
                                 <input type="checkbox" class="custom-control-input" id="customControlInline">
+                                <?php echo $invalido ?>
                             </div>
                         </div>
                         <div class="d-flex justify-content-center mt-3 login_container">
