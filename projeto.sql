d6f9bc4c4cef274c32555e7465171a9d2d3b246b
José Fernandes <zefrancisco2002@gmail.com>
2022-06-12 18:41:14 +0100

    db

--- projeto.sql
+++ projeto.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `projeto` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `projeto`;
 -- MySQL dump 10.13  Distrib 8.0.29, for macos12 (x86_64)
 --
 -- Host: 127.0.0.1    Database: projeto
@@ -265,4 +267,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-12 18:39:59
+-- Dump completed on 2022-06-12 18:41:07
